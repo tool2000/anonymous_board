4f12ffcafba3f0c4e32de88f72dccc58cd308719
tool2000 <shareit@hanmail.net>
2021-11-09 12:06:13 +0900

    morning updated

--- models.py
+++ models.py
@@ -3,8 +3,13 @@ from datetime import datetime
 
 
 class Post(db.Model):
-    
     __tablename__ = 'post'
+
+    id = db.Column(db.Integer, primary_key=True,nullable=False,autoincrement=True)
+    author = db.Column(db.String(256), nullable=False)
+    content = db.Column(db.String(2000), nullable=False)
+    ilike = db.Column(db.Integer, default=0)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __init__(self,author,content):
         self.author = author
